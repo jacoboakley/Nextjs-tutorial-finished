2996cb47b8a393f26a0bdfc68677edf20e70e4a8
jacoboakley <jacoboakley21@gmail.com>
2017-07-16 19:03:02 -0400

    Next.js app on Heroku

---
+++ pages/post.js
@@ -0,0 +1,51 @@
+import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
+
+
+const Post =  (props) => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium}/>
+    <div clssName="markdown">
+      <Markdown source={`
+### Title of comment
+This is some markdown that I am adding
+to input my own comment [link](/link).
+      `} />
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
+  </Layout>
+)
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
