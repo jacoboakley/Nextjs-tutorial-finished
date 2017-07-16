91b2578a0054a251f69337c0f61ff541af5dce2a
jacoboakley <jacoboakley21@gmail.com>
2017-07-16 19:09:47 -0400

    removed additions to about page

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,6 @@ import Layout from '../components/MyLayout.js'
 
 export default () => (
     <Layout>
-       <p>This is the about page it tells us about whatever</p>
+       <p>This is the about page</p>
     </Layout>
 )
